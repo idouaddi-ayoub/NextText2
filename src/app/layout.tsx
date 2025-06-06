cd844c58dd11eca3045e88ed1bf73f83b9dd8821
PrOeZ <idouaddi.ayoub@gmail.com>
2025-06-06 22:24:48 +0100

    ded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
-import "./globals.css";
+import "./styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
